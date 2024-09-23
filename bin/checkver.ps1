f128d395efff992da91e4c16494ad9bbdf3aca18
mattia72 <mattiassicha.tmp@gmail.com>
2024-09-23 08:29:30 +0200

    some commented version query

--- bin/checkver.ps1
+++ bin/checkver.ps1
@@ -3,3 +3,5 @@ if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
 $checkver = "$env:SCOOP_HOME/bin/checkver.ps1"
 $dir = "$psscriptroot/../bucket" # checks the parent dir
 Invoke-Expression -command "$checkver -dir $dir $($args | ForEach-Object { "$_ " })"
+
+#$new_version=git status --short | %{$_ -replace '^M  (.*)', '$1'} | Get-Item | get-content | Select-String -Pattern "`"version`": `"(.*)`""
